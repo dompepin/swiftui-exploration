5999a18691ab99088a3210a4ad3e05b5a0d1a872
dompepin <dompepin@hotmail.com>
2022-02-12 08:27:55 -0700

    Empty Project

---
+++ SwiftUIExploration/SwiftUIExploration/SwiftUIExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExplorationApp.swift
+//  SwiftUIExploration
+//
+//  Created by Dominic Pepin on 2022-02-12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

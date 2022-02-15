2f490ef2a540b39a464e53c8e88a1a0daeec41ef
dompepin <dompepin@hotmail.com>
2022-02-14 20:13:24 -0700

    Fix formatting

--- UICompas/Modules/ActivityIndicators/PulsingActivityIndicator.swift
+++ UICompas/Modules/ActivityIndicators/PulsingActivityIndicator.swift
@@ -77,61 +77,64 @@ public struct PulsingActivityIndicator: View {
 
 extension PulsingActivityIndicator.Configuration {
     static let example_three_shapes: PulsingActivityIndicator.Configuration = .init(views: [AnyView(Circle().fill(Color.red).frame(width:20, height:20)),
-                                                                                AnyView(Triangle().fill(Color.blue).frame(width:20, height:20)),
-                                                                                AnyView(Rectangle().fill(Color.yellow).frame(width:20, height:20))],
-                                                                        spacing: 8,
-                                                                        singlePulseDuration: 0.5,
-                                                                        delay: 0.2,
-                                                                        scale: 0.5,
-                                                                        fade: 0.5)
+                                                                                            AnyView(Triangle().fill(Color.blue).frame(width:20, height:20)),
+                                                                                            AnyView(Rectangle().fill(Color.yellow).frame(width:20, height:20))
+                                                                                           ],
+                                                                                    spacing: 8,
+                                                                                    singlePulseDuration: 0.5,
+                                                                                    delay: 0.2,
+                                                                                    scale: 0.5,
+                                                                                    fade: 0.5)
     
     static let example_text: PulsingActivityIndicator.Configuration = .init(views: [AnyView(Text("L")),
-                                                                                AnyView(Text("O")),
-                                                                                AnyView(Text("A")),
-                                                                                AnyView(Text("D")),
-                                                                                AnyView(Text("I")),
-                                                                                AnyView(Text("N")),
-                                                                                AnyView(Text("G"))],
-                                                                        spacing: 1,
-                                                                        singlePulseDuration: 0.5,
-                                                                        delay: 0.1,
-                                                                        scale: 0.75,
-                                                                        fade: 1)
+                                                                                    AnyView(Text("O")),
+                                                                                    AnyView(Text("A")),
+                                                                                    AnyView(Text("D")),
+                                                                                    AnyView(Text("I")),
+                                                                                    AnyView(Text("N")),
+                                                                                    AnyView(Text("G"))
+                                                                                   ],
+                                                                            spacing: 1,
+                                                                            singlePulseDuration: 0.5,
+                                                                            delay: 0.1,
+                                                                            scale: 0.75,
+                                                                            fade: 1)
     
     static let example_one_view: PulsingActivityIndicator.Configuration = .init(views: [AnyView(Circle().fill(Color.green).frame(width:40, height:40))],
-                                                                        spacing: 1,
-                                                                        singlePulseDuration: 0.5,
-                                                                        delay: 0.01,
-                                                                        scale: 0.75,
-                                                                        fade: 0.2)
+                                                                                spacing: 1,
+                                                                                singlePulseDuration: 0.5,
+                                                                                delay: 0.01,
+                                                                                scale: 0.75,
+                                                                                fade: 0.2)
     
     static let example_shimmer: PulsingActivityIndicator.Configuration = .init(views: (1...200).map({ _ in AnyView(Rectangle().fill(Color.purple).frame(width:1, height:20)) }),
-                                                                        spacing: 0,
-                                                                        singlePulseDuration: 1,
-                                                                        delay: 0.1,
-                                                                        scale: 1,
-                                                                        fade: 0.5)
+                                                                               spacing: 0,
+                                                                               singlePulseDuration: 1,
+                                                                               delay: 0.1,
+                                                                               scale: 1,
+                                                                               fade: 0.5)
     
     static let example_four_circles: PulsingActivityIndicator.Configuration = .init(views: [AnyView(Circle().fill(Color(.sRGB, red: 96.0/255.0, green: 153.0/255.0, blue: 45.0/255.0, opacity: 1.0)).frame(width:20, height:20)),
-                                                                                AnyView(Circle().fill(Color(.sRGB, red: 73.0/255.0, green: 198.0/255.0, blue: 229.0/255.0, opacity: 1.0)).frame(width:20, height:20)),
-                                                                                AnyView(Circle().fill(Color(.sRGB, red: 247.0/255.0, green: 203.0/255.0, blue: 21.0/255.0, opacity: 1.0)).frame(width:20, height:20)),
-                                                                                AnyView(Circle().fill(Color(.sRGB, red: 226.0/255.0, green: 132.0/255.0, blue: 19.0/25.0, opacity: 1.0)).frame(width:20, height:20))],
-                                                                        spacing: 16,
-                                                                        singlePulseDuration: 0.5,
-                                                                        delay: 0.2,
-                                                                        scale: 0.5,
-                                                                        fade: 1.0)
+                                                                                            AnyView(Circle().fill(Color(.sRGB, red: 73.0/255.0, green: 198.0/255.0, blue: 229.0/255.0, opacity: 1.0)).frame(width:20, height:20)),
+                                                                                            AnyView(Circle().fill(Color(.sRGB, red: 247.0/255.0, green: 203.0/255.0, blue: 21.0/255.0, opacity: 1.0)).frame(width:20, height:20)),
+                                                                                            AnyView(Circle().fill(Color(.sRGB, red: 226.0/255.0, green: 132.0/255.0, blue: 19.0/25.0, opacity: 1.0)).frame(width:20, height:20))
+                                                                                           ],
+                                                                                    spacing: 16,
+                                                                                    singlePulseDuration: 0.5,
+                                                                                    delay: 0.2,
+                                                                                    scale: 0.5,
+                                                                                    fade: 1.0)
     
     
     static let example_images: PulsingActivityIndicator.Configuration = .init(views: [AnyView(Image(systemName: "airplane").resizable().foregroundColor(Color.blue).frame(width:10, height:10)),
-                                                                                AnyView(Image(systemName: "airplane").resizable().foregroundColor(Color.blue).frame(width:20, height:20)),
-                                                                                AnyView(Image(systemName: "airplane").resizable().foregroundColor(Color.blue).frame(width:30, height:30)),
-                                                                                ],
-                                                                        spacing: 8,
-                                                                        singlePulseDuration: 0.75,
-                                                                        delay: 0.2,
-                                                                        scale: 0.75,
-                                                                        fade: 0.72)
+                                                                                      AnyView(Image(systemName: "airplane").resizable().foregroundColor(Color.blue).frame(width:20, height:20)),
+                                                                                      AnyView(Image(systemName: "airplane").resizable().foregroundColor(Color.blue).frame(width:30, height:30)),
+                                                                                     ],
+                                                                              spacing: 8,
+                                                                              singlePulseDuration: 0.75,
+                                                                              delay: 0.2,
+                                                                              scale: 0.75,
+                                                                              fade: 0.72)
 }
 
 // MARK: Preview

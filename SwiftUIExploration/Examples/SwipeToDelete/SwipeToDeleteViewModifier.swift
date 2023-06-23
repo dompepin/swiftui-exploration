5890d13e0f26821537f6d07a3531a92b1c871ee4
dompepin <dompepin@hotmail.com>
2023-06-23 08:47:36 -0600

    Slide in the deletion view and auto-calculate the deletion view width

--- SwiftUIExploration/Examples/SwipeToDelete/SwipeToDeleteViewModifier.swift
+++ SwiftUIExploration/Examples/SwipeToDelete/SwipeToDeleteViewModifier.swift
@@ -17,27 +17,26 @@ struct SwipeToDeleteViewModifier: ViewModifier {
     // MARK: Static
     private let backgroundColor: Color = .red
     private let viewId: String
-    private let deletionViewWidth: CGFloat = 50
-    private let minimumSwipeThreshold: CGFloat = 50
     private let swipeToDeleteThresholdRatio: CGFloat = 0.5
         
     // MARK: Properties
-    @State private var contentOffset: CGFloat = 0
-    @State private var contentSize: CGSize = .zero
-    @State private var isDeletionViewShown = false
+    @State private var contentOffset: CGFloat = 0 // The parent view content offset as the user is dragging it
     @State private var lastOffset: CGFloat = 0 // Used when starting to drag from a visible state
-    @State private var isDeleting: Bool = false
+    @State private var contentSize: CGSize = .zero // The size of the row being swiped
+    @State private var deletionViewSize: CGSize = .zero // The size of the deletion view tap area
+    @State private var isDeletionViewShown = false // Indicates if the deletion view is shown
+    @State private var isDeleting: Bool = false // Indicates if the delete animation is running
     
     private var onDelete: () -> Void
     
-    // Used to ensure that only one view is shown at any time
+    // Stores the id of the view that is currently swiped.
+    // This is used to ensure that only one view is shown at any time.
+    // Each row will listen to change and if it's not their id being stored, they will colapse
     @AppStorage("ViewSync.SwipeToDeleteViewId") var swipeToDeleteViewId: String = ""
-    
-    
+        
     // MARK: Body
-    
     func body(content: Content) -> some View {
-        ZStack(alignment: .trailing) {
+        ZStack() {
             deletionView()
             contentView(content)
         }
@@ -67,10 +66,9 @@ struct SwipeToDeleteViewModifier: ViewModifier {
                         if isDeleting {
                             return
                         }
-                        print("View: '\(viewId)' -> lastOffset: \(lastOffset)")
                         swipeToDeleteViewId = viewId
                         contentOffset = calculateContentOffset(translationWidth: value.translation.width, dragStartingOffset: lastOffset)
-                        isDeletionViewShown = contentOffset < -minimumSwipeThreshold
+                        isDeletionViewShown = contentOffset < -deletionViewSize.width
                         if contentOffset < -swipeToDeleteThreshold() {
                             deleteItem()
                         }
@@ -81,10 +79,12 @@ struct SwipeToDeleteViewModifier: ViewModifier {
                         }
                         if isDeletionViewShown {
                             withAnimation {
-                                contentOffset = -deletionViewWidth
+                                // Show the deletion view properly
+                                contentOffset = -deletionViewSize.width
                             }
                         } else {
                             withAnimation {
+                                // Hide the deletion view
                                 contentOffset = 0
                             }
                         }
@@ -94,25 +94,32 @@ struct SwipeToDeleteViewModifier: ViewModifier {
     }
     
     private func deletionView() -> some View {
-        VStack {
-            Spacer()
-            HStack {
-                Spacer()
-                Image(systemName: "trash")
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(width: deletionViewWidth)
-                    .contentShape(Rectangle())
-                    .onTapGesture {
-                        deleteItem()
-                    }
-            }
-            Spacer()
-        }
-        .background {
-            Color.red
+        ZStack {
+            deletionViewBackground()
+                .offset(x: contentSize.width + contentOffset)
+            deletionViewForeground()
+                .offset(x: max(contentSize.width + contentOffset, contentSize.width - deletionViewSize.width))
         }
         .frame(height: contentSize.height)
+    }
+    
+    private func deletionViewForeground() -> some View {
+        HStack {
+            Image(systemName: "trash")
+                .font(.title2)
+                .foregroundColor(.white)
+                .padding(.horizontal, 30)
+                .getSize($deletionViewSize)
+            Spacer()
+        }
+        .contentShape(Rectangle())
+        .onTapGesture {
+            deleteItem()
+        }
+    }
+    
+    private func deletionViewBackground() -> some View {
+        backgroundColor
     }
 
     // MARK: Private methods
@@ -127,7 +134,6 @@ struct SwipeToDeleteViewModifier: ViewModifier {
     
     private func swipeToDeleteThreshold() -> CGFloat {
         let value = contentSize.width == 0 ? 240 : contentSize.width * swipeToDeleteThresholdRatio
-        print("Content Width \(value)")
         return value
     }
     
@@ -140,7 +146,6 @@ struct SwipeToDeleteViewModifier: ViewModifier {
     }
     
     private func resetView() {
-        print("Resetting View: '\(viewId)'")
         withAnimation {
             contentOffset = 0
             lastOffset = 0
